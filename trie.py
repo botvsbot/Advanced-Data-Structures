c33ad6806398b6a7ee3a03e81854dbcb7e71f1d4
Rajaganesh Ganesh <g.rajaganesh@gmail.com>
2014-12-05 13:28:27 -0800

    Create trie.py

---
+++ trie.py
@@ -0,0 +1,57 @@
+# A basic trie with insert, search and delete operations
+
+import time
+
+class Trie(object):
+
+	def __init__(self):
+		self.word = None
+		self.children = {}
+
+	def insert(self, word):
+		for letter in word.lower():
+			if letter not in self.children:
+				self.children[letter] = Trie()
+			self = self.children[letter]
+		self.word = word
+
+	def search(self, word):
+		for letter in word.lower():
+			if letter not in self.children:
+				return False
+			self = self.children[letter]
+		return (self.word != None)
+
+	def delete(self, word):
+		deepest_tree = self
+		suffix = ""
+		for letter in word.lower():
+			if letter not in self.children:
+				return # the word to be deleted is not in the trie
+			if self.word != None: # A possible prefix exists to the word to be deleted
+				deepest_tree = self
+				suffix = letter
+			else:
+				suffix = suffix+letter
+			self = self.children[letter]
+		if self.word == word:
+			if not self.children: # the word is not a prefix of an existing word
+				for i in xrange(0, len(suffix)-1):
+					if i < len(suffix)-2:
+						grand_child = (deepest_tree.children[suffix[i]]).children[suffix[i+1]]
+						deepest_tree.children[suffix[i+1]] = grand_child 
+					del(deepest_tree.children[suffix[i]])
+						
+			else: # the word is a prefix of another word in the trie
+				self.word = None
+		else:
+			return # the word to be deleted is not in the trie
+
+
+trie = Trie()
+words = "the their there they hi sup know knowing".split(" ")
+for word in words:
+  trie.insert(word)
+trie.search("there")
+trie.delete("there")
+trie.search("they")
